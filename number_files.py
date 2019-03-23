fe81a94c850ce4bd345a1d77a2dc2dccab62ecd5
Sulaiman Sanaullah <ss7@illinois.edu>
2019-03-23 01:56:10 -0500

    update formatting and nomenclature for clarity

--- number_files.py
+++ number_files.py
@@ -1,24 +1,21 @@
 def number_files(dir_path):
     """
-    Numbers all files in a directory starting
-    from zero. Retains extensions.
+    Numbers all files in an arbitrary directory
+    starting from zero. Retains extensions.
     """
     files = os.listdir(dir_path)
 
     for index in range(len(files)):
 
-        file_name = files[index]
-        file_root = file_name.split('.')[0]
-        file_ext  = file_name.split('.')[1]
+        current_file_name = files[index]
+        current_file_root = current_file_name.split('.')[0]
+        file_ext          = current_file_name.split('.')[1]
+        
+        desired_file_name = current_file_root + str(index) + '.' + file_ext
 
-        current_path = os.path.join(dir_path,
-                file_name)
-        desired_path = os.path.join(dir_path,
-                file_root  +\
-                str(index) +\
-                '.'        +\
-                file_ext)
-
+        current_path      = os.path.join(dir_path, current_file_name)
+        desired_path      = os.path.join(dir_path, desired_file_name)
+        
         os.rename(current_path, desired_path)
         print(current_path, '=>', desired_path)
 
